c0eee771dd92ac1837abba4e9bd4677d2eb01cad
developer <shpandey@bestpeers.com>
2023-07-14 14:24:27 +0530

    Added permit in user model and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_no, :role, :block_status])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :contact_no, :role, :block_status])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :first_name, :last_name, :contact_no, :role, :block_status
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
