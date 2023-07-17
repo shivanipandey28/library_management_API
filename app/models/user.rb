efd3266c69d568e41a9a9d9ec46ba938e92610ea
developer <shpandey@bestpeers.com>
2023-07-17 11:31:07 +0530

    C

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
   has_many :books, dependent: :destroy
+  has_many :user_books, dependent: :destroy
+  enum role: { admin: "admin", user: "user" }
+
+  def set_default_role
+    self.role ||= 'user'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+  end
 end
